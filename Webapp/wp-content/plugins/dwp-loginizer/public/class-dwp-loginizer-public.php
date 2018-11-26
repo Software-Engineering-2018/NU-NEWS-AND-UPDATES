88dad5e17b6c4d318dd152f95b0c49422f785da8
Biren Sharma <31134009+birensharma@users.noreply.github.com>
2018-11-27 00:22:07 +0530

    next commit

---
+++ Webapp/wp-content/plugins/dwp-loginizer/public/class-dwp-loginizer-public.php
@@ -0,0 +1,126 @@
+<?php
+
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * @link       ''
+ * @since      1.0.0
+ *
+ * @package    Dwp_Loginizer
+ * @subpackage Dwp_Loginizer/public
+ */
+
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the admin-specific stylesheet and JavaScript.
+ *
+ * @package    Dwp_Loginizer
+ * @subpackage Dwp_Loginizer/public
+ * @author     Demos Palana <demospalana@yahoo.com>
+ */
+class Dwp_Loginizer_Public {
+
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $plugin_name    The ID of this plugin.
+	 */
+	private $plugin_name;
+
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $version    The current version of this plugin.
+	 */
+	private $version;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 * @param      string    $plugin_name       The name of the plugin.
+	 * @param      string    $version    The version of this plugin.
+	 */
+	public function __construct( $plugin_name, $version ) {
+
+		$this->plugin_name = $plugin_name;
+		$this->version = $version;
+
+	}
+
+	/**
+	 * Register the stylesheets for the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_styles() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Dwp_Loginizer_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Dwp_Loginizer_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/dwp-loginizer-public.css', array(), $this->version, 'all' );
+
+	}
+
+	/**
+	 * Register the JavaScript for the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_scripts() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Dwp_Loginizer_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Dwp_Loginizer_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/dwp-loginizer-public.js', array( 'jquery' ), $this->version, false );
+
+	}
+
+	/**
+	 * Register the stylesheets for the login page.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_login_scripts() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Dwp_Loginizer_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Dwp_Loginizer_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/dwp-loginizer-login.css', array(), $this->version, 'all' );
+
+	}
+
+}
